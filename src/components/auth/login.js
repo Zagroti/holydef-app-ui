ad0aee486e8459fc7f037dfbb5b784b340d070e1
aras armani <aras.armani2014@gmail.com>
2018-09-19 15:46:40 +0300

    background image login pages is addded

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text , StyleSheet, ImageBackground } from 'react-native';
 
 class Login extends Component {
     constructor(props) {
@@ -9,11 +9,26 @@ class Login extends Component {
     render() { 
         return ( 
 
-            <View>
+            <ImageBackground source={require('../../assets/img/login.png')} style={ styles.backgroundImage } >
                 <Text> Login page is OK</Text>
-            </View>
+            </ImageBackground>
          );
     }
 }
+
+
+const styles = StyleSheet.create({
+    container:{
+        padding: 20,
+
+    },
+    backgroundImage: {
+        width: '100%',
+        height: '100%', 
+        flex:1,
+  
+    },
+
+});
  
 export default Login;
