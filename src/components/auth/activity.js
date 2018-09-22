60249fb0b15190f8d7de04d7ff746808f3c88735
aras armani <aras.armani2014@gmail.com>
2018-09-22 09:24:58 +0300

    activity pages is added

---
+++ src/components/auth/activity.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Activity extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+
+            <View>
+                <Text>Activity page si ready </Text>
+            </View>
+         );
+    }
+}
+ 
+export default Activity;
