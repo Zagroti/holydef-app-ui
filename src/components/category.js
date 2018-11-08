b63246a3b90831a4357525e13fd06df8e708d3a3
aras armani <aras.armani2014@gmail.com>
2018-11-08 16:53:10 +0300

    review code

--- src/components/category.js
+++ src/components/category.js
@@ -64,27 +64,27 @@ class category extends Component {
  
         if(id ==0)
         this.setState({imageFile : img01});
-        else if(id ===1)
+            else if(id ===1)
         this.setState({imageFile : img02});
-        else if(id ===2)
+            else if(id ===2)
         this.setState({imageFile : img03});
-        else if(id ===3)
+            else if(id ===3)
         this.setState({imageFile : img04});
-        else if(id ===4)
+            else if(id ===4)
         this.setState({imageFile : img05});
-        else if(id ===5)
+            else if(id ===5)
         this.setState({imageFile : img06});
-        else if(id ===6)
+            else if(id ===6)
         this.setState({imageFile : img07});
-        else if(id ===7)
+            else if(id ===7)
         this.setState({imageFile : img08});
-        else if(id ===8)
+            else if(id ===8)
         this.setState({imageFile : img09});
-        else if(id ===9)
+            else if(id ===9)
         this.setState({imageFile : img10});
-        else if(id ===10)
+            else if(id ===10)
         this.setState({imageFile : img11});
-        else if(id ===11)
+            else if(id ===11)
         this.setState({imageFile : img12});
     }
     _loadInitialState = async () => {
